4145e0f159372020569b41fccb9311b15e948e3c
vishnu <satheesh.vishnu2@gmail.com>
2022-11-22 23:11:30 -0500

    Find winner who wins max times in 5 rounds
    Add code to do five rounds and declare the final winner based on the rsult for each round
    Also added a check to ensure user chooses a valid option among rock/paper/scissor
    
    	modified:   javascript.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,66 @@
 const userOptions = ["rock","paper","scissor"];
 
-let computerChoice = getComputerChoice(userOptions);
+let countComputerWins = 0;
+let countUserWins = 0;
+let winner = "";
 
-let playerSelection= prompt("What are you selecting?? Type in \"Stone\", \"Paper\", \"Scissor\"").toLowerCase();
 
-console.log(`Computer selection: ${computerChoice}`);
-console.log(`Player selection: ${playerSelection}`);
+for(let i = 0;i<5;i++){
 
-singleRoundResult(playerSelection,computerChoice);
+    let computerChoice = getComputerChoice(userOptions);
+
+    let playerSelection= prompt("What are you selecting?? Type in \"Rock\", \"Paper\", \"Scissor\"").toLowerCase();
+
+    while(playerSelection!="rock"&&playerSelection!="paper"&&playerSelection!="scissor"){
+        alert("Invalid Entry");
+
+        playerSelection = prompt("What are you selecting?? Type in \"Rock\", \"Paper\", \"Scissor\"").toLowerCase();
+    }
+
+    console.log(`Computer selection: ${computerChoice}`);
+    console.log(`Player selection: ${playerSelection}`);
+
+    result = singleRoundResult(playerSelection,computerChoice);
+
+    if( result === "tie"){
+        continue;
+    }
+
+    if(result === "computer"){
+        countComputerWins+=1;
+    }
+
+    if(result === "user"){
+        countUserWins+=1;
+    }
+
+    if(countComputerWins===3){
+        console.log("Oopss!!! You lost!! Better luck next time");
+        break;
+
+    }
+
+    if(countUserWins===3){
+        console.log("Wohoooo!!! YOU WON!!!!");
+        break;
+    }
+
+    if(i===4){
+        if(countComputerWins>countUserWins){
+            console.log("Oopss!!! You lost!! Better luck next time");
+        }
+
+        else if(countComputerWins<countUserWins){
+            console.log("Wohoooo!!! YOU WON!!!!");
+        }
+
+        else{
+            console.log("Its a tie!!!");
+        }
+    }
+}
+
+
 
 
 function getComputerChoice(userOptions){
@@ -23,38 +76,45 @@ function singleRoundResult(playerSelection,computerChoice){
 
     if(playerSelection===computerChoice){
         console.log("Its a tie!!!!!!");
+        return winner = "tie";
     }
     
     else{
 
         if(playerSelection==="rock"){
             if(computerChoice==="scissor"){
-                console.log("You win!!! Rock beats Scissor");
+               console.log("You win!!!");
+                return winner = "user";
             }
 
             else{
-                console.log("Oops!! You lose!!! Paper beats Rock");
+               console.log("Computer win!!!");
+                return winner = "computer";
             }
         }
 
         else if(playerSelection==="paper"){
 
             if(computerChoice==="rock"){
-                console.log("You win!!! Paper beats Rock");
+               console.log("You win!!!");
+                return winner = "user";
             }
 
             else{
-                console.log("Oops!! You lose!!! Scissor beats Paper");
+               console.log("Computer win!!!");
+                return winner = "computer";
             }
         }
 
         else if(playerSelection==="scissor"){
             if(computerChoice==="paper"){
-                console.log("You win!!! Scissor beats Paper");
+              console.log("You win!!!");
+                return winner = "user";
             }
 
             else{
-                console.log("Oops!! You lose!!! Rock beats Paper");
+              console.log("Computer win!!!");
+                return winner = "computer";
             }
         }
 
