74cceff6d873d67a0e53ef31f88121b8fc227f97
Vishnu <satheesh.vishnu2@gmail.com>
2022-11-29 23:45:01 -0500

    Add UI for the game
    >Created a landing page with a button to let users start the game
    > Created another html file with barebone UI which shows the current player and options for users to select.

---
+++ dom.js
@@ -0,0 +1,70 @@
+
+// const para1 = document.createElement('p');
+
+// para1.textContent = "Hey I'm red!";
+
+// para1.style.cssText = 'color:red;';
+
+// const heading3 = document.createElement('h3');
+
+// heading3.textContent = "I’m a blue h3!";
+
+// heading3.style.cssText = 'color:blue;';
+
+// const div = document.createElement('div');
+
+// const para2 = document.createElement('p');
+
+// para2.textContent = "ME TOO!";
+
+// const heading1 = document.createElement('h1');
+
+// heading1.textContent = "I’m in a div";
+
+// document.body.appendChild(para1);
+// document.body.appendChild(heading3);
+// document.body.appendChild(div);
+
+// div.appendChild(para2);
+// div.appendChild(heading1);
+
+// div.classList.add("new");
+
+
+
+
+
+
+
+
+
+
+
+
+// console.dir(document);
+
+// var headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
+// headerTitle.textContent = "Hello";
+
+
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items[0]);
+
+//Traversing the nodes
+// //parent node
+var itemList = document.querySelector('#items')
+// console.log(itemList.parentNode)
+
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+
+
+// Parent element
+
+// same as parent node
+
+//child nodes
+
+// console.log(itemList.childNodes)
+
+console.log(itemList.children)
