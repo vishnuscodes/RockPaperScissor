44cbaede24787e887b9d087c10cdd27306f181e3
Vishnu <satheesh.vishnu2@gmail.com>
2022-12-02 02:11:47 -0500

    Add UI for the game_page
    >Added a score board
    >Added buttons for users to select their option
    >Displays option selected by the computer
    >Exit button to go back to landing page and resets score board when the game is started again.
    
    	new file:   .vscode/settings.json
    	modified:   game_page.css
    	modified:   game_page.html
    	new file:   images/baseline_monitor_black_24dp.png
    	new file:   images/baseline_screenshot_monitor_black_24dp.png
    	new file:   images/computer-icon.png
    	new file:   images/landscape_FILL0_wght400_GRAD0_opsz48.png
    	new file:   images/rock.png
    	new file:   images/user-icon.png
    	modified:   javascript.js
    	new file:   screenshot_monitor_black_24dp.svg

--- javascript.js
+++ javascript.js
@@ -5,69 +5,168 @@ let countUserWins = 0;
 let winner = "";
 
 
-for(let i = 0;i<5;i++){
+// for(let i = 0;i<5;i++){
 
+    // let computerChoice = getComputerChoice(userOptions);
+    let playerSelection = "";
+    // let userClicked1 = false;
+    // let userClicked2 = false;
+    // let userClicked3 = false;
+    // let letComputerSelect = true;
+
+  
+        // console.log("Inside loop")
+        const btn1 = document.getElementById("option1");
+        const btn2 = document.getElementById("option2");
+        const btn3 = document.getElementById("option3");
+        const exit = document.getElementById("exit");
+
+        btn1.addEventListener("click", Option1);
+    
+        btn2.addEventListener("click", Option2);
+    
+        btn3.addEventListener("click", Option3);
+
+        exit.addEventListener("click", exitFunction);
+
+
+   
+    // while(letComputerSelect){
+        // if(userClicked1===true||userClicked2===true||userClicked3===true){
+            // let computerChoice = getComputerChoice(userOptions);
+        //     letComputerSelect==false;
+        // }
+        
+    // }
+
+    // lplayerSelection= prompt("What are you selecting?? Type in \"Rock\", \"Paper\", \"Scissor\"").toLowerCase();
+
+    // while(playerSelection!="rock"&&playerSelection!="paper"&&playerSelection!="scissor"){
+    //     alert("Invalid Entry");
+
+    //     playerSelection = prompt("What are you selecting?? Type in \"Rock\", \"Paper\", \"Scissor\"").toLowerCase();
+    // }
+
+    // console.log(`Computer selection: ${computerChoice}`);
+    // console.log(`Player selection: ${playerSelection}`);
+
+    // result = singleRoundResult(playerSelection,computerChoice);
+
+    // if( result === "tie"){
+    //     continue;
+    // }
+
+    // if(result === "computer"){
+    //     countComputerWins+=1;
+    //     document.getElementById("computer-score").textContent = countComputerWins;
+    //     console.log(  document.getElementById("computer-score").textContent);
+    // }
+
+    // if(result === "user"){
+    //     countUserWins+=1;
+    //     document.getElementById("user-score").textContent = countUserWins;
+    //     console.log(  document.getElementById("user-score").textContent);
+    // }
+
+    // if(countComputerWins===3){
+    //     console.log("Oopss!!! You lost!! Better luck next time");
+    //     // break;
+    // }
+
+    // if(countUserWins===3){
+    //     console.log("Wohoooo!!! YOU WON!!!!");
+    //     // break;
+    // }
+
+    // if(i===4){
+    //     if(countComputerWins>countUserWins){
+    //         console.log("Oopss!!! You lost!! Better luck next time");
+    //     }
+
+    //     else if(countComputerWins<countUserWins){
+    //         console.log("Wohoooo!!! YOU WON!!!!");
+    //     }
+
+    //     else{
+    //         console.log("Its a tie!!!");
+    //     }
+    // }
+// }
+
+function Option1(){
+    playerSelection = "rock";
+    console.log(`Player Selection ${playerSelection}`);
     let computerChoice = getComputerChoice(userOptions);
-
-    let playerSelection= prompt("What are you selecting?? Type in \"Rock\", \"Paper\", \"Scissor\"").toLowerCase();
-
-    while(playerSelection!="rock"&&playerSelection!="paper"&&playerSelection!="scissor"){
-        alert("Invalid Entry");
-
-        playerSelection = prompt("What are you selecting?? Type in \"Rock\", \"Paper\", \"Scissor\"").toLowerCase();
-    }
-
-    console.log(`Computer selection: ${computerChoice}`);
-    console.log(`Player selection: ${playerSelection}`);
-
+    document.getElementById("display-computer-selection").textContent = computerChoice;
     result = singleRoundResult(playerSelection,computerChoice);
-
-    if( result === "tie"){
-        continue;
-    }
-
     if(result === "computer"){
         countComputerWins+=1;
+        document.getElementById("computer-score").textContent = countComputerWins;
+        // console.log(  document.getElementById("computer-score").textContent);
     }
 
     if(result === "user"){
         countUserWins+=1;
+        document.getElementById("user-score").textContent = countUserWins;
+        // console.log(  document.getElementById("user-score").textContent);
     }
-
-    if(countComputerWins===3){
-        console.log("Oopss!!! You lost!! Better luck next time");
-        break;
-
-    }
-
-    if(countUserWins===3){
-        console.log("Wohoooo!!! YOU WON!!!!");
-        break;
-    }
-
-    if(i===4){
-        if(countComputerWins>countUserWins){
-            console.log("Oopss!!! You lost!! Better luck next time");
-        }
-
-        else if(countComputerWins<countUserWins){
-            console.log("Wohoooo!!! YOU WON!!!!");
-        }
-
-        else{
-            console.log("Its a tie!!!");
-        }
-    }
+    // userClicked1 = true;
+    // console.log("Option1");
 }
 
+function Option2(){
+    playerSelection = "paper";
+    console.log(`Player Selection  ${playerSelection}`);
+    let computerChoice = getComputerChoice(userOptions);
+    document.getElementById("display-computer-selection").textContent = computerChoice;
+    result = singleRoundResult(playerSelection,computerChoice);
+    if(result === "computer"){
+        countComputerWins+=1;
+        document.getElementById("computer-score").textContent = countComputerWins;
+        console.log(  document.getElementById("computer-score").textContent);
+    }
 
+    if(result === "user"){
+        countUserWins+=1;
+        document.getElementById("user-score").textContent = countUserWins;
+        console.log(  document.getElementById("user-score").textContent);
+    }
+    // userClicked2 = true;
+    // console.log("Option2");
+}
 
+function Option3(){
+    playerSelection = "scissor";
+    console.log(`Player Selection ${playerSelection}`);
+    let computerChoice = getComputerChoice(userOptions);
+    document.getElementById("display-computer-selection").textContent = computerChoice;
+    result = singleRoundResult(playerSelection,computerChoice);
+    if(result === "computer"){
+        countComputerWins+=1;
+        document.getElementById("computer-score").textContent = countComputerWins;
+        console.log(  document.getElementById("computer-score").textContent);
+    }
+
+    if(result === "user"){
+        countUserWins+=1;
+        document.getElementById("user-score").textContent = countUserWins;
+        console.log(  document.getElementById("user-score").textContent);
+    }
+    // userClicked3 = true;
+    // console.log("Option3");
+}
+
+function exitFunction(){
+    window.location.href = "index.html";
+}
 
 function getComputerChoice(userOptions){
     //const optionsForComputer = ["Rock","Paper","Scissor"];
     //return optionsForComputer[0];
+    // console.log("getting computer choice")
     let limit = (userOptions.length);
     let computerSelection =  userOptions[Math.floor(Math.random()*limit)];
+    console.log(computerSelection);
     return computerSelection;
 }
 
